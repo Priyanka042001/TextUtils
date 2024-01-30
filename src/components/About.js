ff56534182e56264e6150bd44481d8ab7f3dc8c2
Priyanka042001 <116100496+Priyanka042001@users.noreply.github.com>
2024-01-30 22:18:52 +0530

    add changes

--- src/components/About.js
+++ src/components/About.js
@@ -9,37 +9,40 @@ export default function About(props) {
       <div className="accordion" id="accordionExample"  >
   <div className="accordion-item" >
     <h2 className="accordion-header">
-      <button className="accordion-button" style={{backgroundColor: props.mode === "dark"?"grey":"white",color:props.mode === "dark"?"white":"black"}} type="button" data-bs-toggle="collapse" data-bs-target="#collapseOne" aria-expanded="true" aria-controls="collapseOne">
-        Accordion Item #1
+      <button className="accordion-button" style={{backgroundColor: props.mode === "dark"?"#80808073":"white",color:props.mode === "dark"?"white":"black"}} type="button" data-bs-toggle="collapse" data-bs-target="#collapseOne" aria-expanded="true" aria-controls="collapseOne">
+        <strong>About Us </strong>
       </button>
     </h2>
     <div id="collapseOne" className="accordion-collapse collapse show" data-bs-parent="#accordionExample">
       <div className="accordion-body" style={{backgroundColor: props.mode === "dark"?"grey":"white",color:props.mode === "dark"?"white":"black"}}>
-        <strong>This is the first item's accordion body.</strong> It is shown by default, until the collapse plugin adds the appropriate classes that we use to style each element. These classes control the overall appearance, as well as the showing and hiding via CSS transitions. You can modify any of this with custom CSS or overriding our default variables. It's also worth noting that just about any HTML can go within the <code>.accordion-body</code>, though the transition does limit overflow.
+         At TextUtils, our mission is to empower users with powerful yet user-friendly text manipulation tools. Whether you're a writer, developer, or just someone looking to enhance your text, TextUtils is here to assist you.   If you're looking to transform text into uppercase or lowercase, [TextUtils] has you covered. Our user-friendly website provides seamless functionality to convert text, giving you the flexibility you need. Explore the power of transformation with [TextUtils] and enhance your textual experience.
+
+Feel free to visit TextUtils and try out our text conversion tools today!
       </div>
     </div>
   </div>
   <div className="accordion-item">
     <h2 className="accordion-header">
-      <button className="accordion-button collapsed" style={{backgroundColor: props.mode === "dark"?"grey":"white",color:props.mode === "dark"?"white":"black"}}type="button" data-bs-toggle="collapse" data-bs-target="#collapseTwo" aria-expanded="false" aria-controls="collapseTwo">
-        Accordion Item #2
+      <button className="accordion-button collapsed" style={{backgroundColor: props.mode === "dark"?"#80808073":"white",color:props.mode === "dark"?"white":"black"}}type="button" data-bs-toggle="collapse" data-bs-target="#collapseTwo" aria-expanded="false" aria-controls="collapseTwo">
+        <strong>Our Team</strong>
       </button>
     </h2>
     <div id="collapseTwo" className="accordion-collapse collapse" data-bs-parent="#accordionExample">
       <div className="accordion-body" style={{backgroundColor: props.mode === "dark"?"grey":"white",color:props.mode === "dark"?"white":"black"}}>
-        <strong>This is the second item's accordion body.</strong> It is hidden by default, until the collapse plugin adds the appropriate classes that we use to style each element. These classes control the overall appearance, as well as the showing and hiding via CSS transitions. You can modify any of this with custom CSS or overriding our default variables. It's also worth noting that just about any HTML can go within the <code>.accordion-body</code>, though the transition does limit overflow.
+      At the core of TextUtils is our diverse and talented team. Comprising experts in linguistic technology, software development, and user experience, we collaborate to bring you cutting-edge text transformation tools.
       </div>
     </div>
   </div>
   <div className="accordion-item">
     <h2 className="accordion-header">
-      <button className="accordion-button collapsed" style={{backgroundColor: props.mode === "dark"?"grey":"white",color:props.mode === "dark"?"white":"black"}}type="button" data-bs-toggle="collapse" data-bs-target="#collapseThree" aria-expanded="false" aria-controls="collapseThree">
-        Accordion Item #3
+      <button className="accordion-button collapsed" style={{backgroundColor: props.mode === "dark"?"#80808073":"white",color:props.mode === "dark"?"white":"black"}}type="button" data-bs-toggle="collapse" data-bs-target="#collapseThree" aria-expanded="false" aria-controls="collapseThree">
+        <strong>Why Choose Us</strong>
       </button>
     </h2>
     <div id="collapseThree" className="accordion-collapse collapse" data-bs-parent="#accordionExample">
       <div className="accordion-body" style={{backgroundColor: props.mode === "dark"?"grey":"white",color:props.mode === "dark"?"white":"black"}}>
-        <strong>This is the third item's accordion body.</strong> It is hidden by default, until the collapse plugin adds the appropriate classes that we use to style each element. These classes control the overall appearance, as well as the showing and hiding via CSS transitions. You can modify any of this with custom CSS or overriding our default variables. It's also worth noting that just about any HTML can go within the <code>.accordion-body</code>, though the transition does limit overflow.
+      Choosing TextUtils means choosing unparalleled quality, reliability, and innovation in text manipulation. Discover the reasons why our users trust us for a range of essential text transformation services.
+We're committed to ensuring customer satisfaction, providing excellence in every interaction, and delivering user-friendly solutions for all your text-related needs. Join the TextUtils community and experience the difference in text transformation.
       </div>
     </div>
   </div>
--- src/components/Textform.js
+++ src/components/Textform.js
@@ -25,18 +25,18 @@ export default function Textform(props) {
     <>
     
       <div className="container" style={{color:props.mode === "dark"?"white":"black"}}>
-        <h1>{props.heading} </h1>
+        <h1 className='mb-4'>{props.heading} </h1>
         <div className="mb-3" >
-          <textarea className="form-control" value={text} onChange={handleOnChange} id="mybox" rows="8" column="5" style={{backgroundColor: props.mode === "dark"?"grey":"white",color:props.mode === "dark"?"white":"black"}}></textarea>
+          <textarea className="form-control" value={text} onChange={handleOnChange} id="mybox" rows="8" column="5" style={{backgroundColor: props.mode === "dark"?"#35232378":"white",color:props.mode === "dark"?"white":"black"}}></textarea>
         </div>
-        <button className="btn  mx-2" onClick={handleUpClick} style={{backgroundColor: props.mode === "dark"?"black":"grey",color:props.mode === "dark"?"white":"black"}} >Convert Text into Uppercase</button>
-        <button className="btn  mx-2" onClick={handleLoClick} style={{backgroundColor: props.mode === "dark"?"black":"grey",color:props.mode === "dark"?"white":"black"}}>Convert Text into Lowercase</button>
+        <button disabled= {text.length===0} className="btn  mx-2 my-1" onClick={handleUpClick} style={{backgroundColor: props.mode === "dark"?"black":"grey",color:props.mode === "dark"?"white":"black"}} >Convert Text into Uppercase</button>
+        <button disabled= {text.length===0} className="btn  mx-2 my-1" onClick={handleLoClick} style={{backgroundColor: props.mode === "dark"?"black":"grey",color:props.mode === "dark"?"white":"black"}}>Convert Text into Lowercase</button>
 
       </div>
       <div className="containers" style={{color:props.mode === "dark"?"white":"black"}}>
         <h2>Your Text Summary</h2>
-        <p>{text.split(" ").length} words and {text.length} characters</p>
-        <p>{0.08 * text.split(" ").length} Minutes read</p>
+        <p>{text.split(" ").filter((element) =>{ return element.length!==0}).length} words and {text.length} characters</p>
+        <p>{0.08 * text.split(" ").filter((element) =>{ return element.length!==0}).length} Minutes read</p>
         <h2>Preview</h2>
         <p>{text}</p>
       </div>
